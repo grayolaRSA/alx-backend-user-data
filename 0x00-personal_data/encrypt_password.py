626ae0582d834f4b35e1e5b971462c375f611f69
grayolaRSA <gtndanga@gmail.com>
2023-11-04 17:09:05 +0200

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,9 +3,15 @@
 
 
 import bcrypt
-from bcrypt import hashpw
+from bcrypt import hashpw, checkpw
 
 
 def hash_password(password: str) -> bytes:
     """function that hashes passwords"""
+
     return hashpw(b"password", bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if password is valid"""
+    return checkpw(b"password", hashed_password)
