8fae4226600ff9a15b9305729e6721eb1d410d2b
grayolaRSA <gtndanga@gmail.com>
2023-11-06 22:34:33 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,18 +5,20 @@
 from flask import request
 from typing import List, TypeVar
 
-class Auth:
 
-    def require_auth(self, path:str, excluded_paths: List[str]) -> bool:
+class Auth:
+    """Basic Authorization class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """method that returns False
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """method that returns None when flask request made
         """
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None when flask request made
