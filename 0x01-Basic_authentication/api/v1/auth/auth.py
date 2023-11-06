3dc5c2604a2c001a90a1f1940700b3129d027001
grayolaRSA <gtndanga@gmail.com>
2023-11-06 22:57:08 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,22 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """method that returns False
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        expected_path = path if path.endswith('/') else path + '/'
+
+        for excluded_path in excluded_paths:
+            normalized_excluded = excluded_path if excluded_path.endswith('/')\
+                 else excluded_path + '/'
+
+            if expected_path.startswith(normalized_excluded):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """method that returns None when flask request made
