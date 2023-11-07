9fb0b114dc98e342432c259cc72402753f7ee567
grayolaRSA <gtndanga@gmail.com>
2023-11-07 20:59:29 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,21 @@ class BasicAuth(Auth):
         if authorization_header.__str__ is None:
             return None
         return str(authorization_header)[len("Basic "):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """ method that decodes the authorization header and gives the username
+        and password string originally input
+        """
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            base64_bytes_decoded = base64.b64decode(base64_bytes)
+
+            return base64_bytes_decoded.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
