b684e475df8e8bec17b1bcae458a3045cc560dbf
grayolaRSA <gtndanga@gmail.com>
2023-11-07 20:17:43 +0200

    update

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""module for Basic Auth"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class for Basic authorisation that is based on base authorisation class
+    """
+    pass
