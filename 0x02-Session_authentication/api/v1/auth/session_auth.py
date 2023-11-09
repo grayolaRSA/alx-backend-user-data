5a4282cd321d4e4abb221306fe311600f2e9be77
grayolaRSA <gtndanga@gmail.com>
2023-11-09 22:00:49 +0200

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,15 +21,15 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str) or user_id is None:
             return None
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id = {session_id: user_id}
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
     
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         method to get a user ID related to a session ID
         """
-        if not isinstance(session_id, str) or session_id is None:
+        if session_id is None or not isinstance(session_id, str):
             return None
         # user_id_by_session_id = {}
         # for session_id, v in user_id_by_session_id:
-        return self.user_id_by_session_id.get(session_id, None)
+        return self.user_id_by_session_id.get(session_id)
