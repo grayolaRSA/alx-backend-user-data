64ad02721dc2804ed14ccf1c4c4d71f42be179f1
grayolaRSA <gtndanga@gmail.com>
2023-11-09 21:28:40 +0200

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-"""Basic authorization module"""
+"""Session authorization module"""
 
 
 from flask import request
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """Basic Authorization class
+    """Session Authorization class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        method that creates a session ID for a user ID
+        """
+
+        if not isinstance(user_id, str) or user_id is None:
+            return None
+        session_id = uuid.uuid4()
+        user_id_by_session_id = {session_id: user_id}
+        return user_id_by_session_id
