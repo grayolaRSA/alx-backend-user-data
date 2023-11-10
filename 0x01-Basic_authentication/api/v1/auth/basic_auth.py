20f5046bdede2b26a38a7d2dc74df789bfe718f0
grayolaRSA <gtndanga@gmail.com>
2023-11-10 18:55:18 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,7 +54,7 @@ class BasicAuth(Auth):
             return None, None
         if ":" not in decoded_base64_authorization_header:
             return None, None
-        result = decoded_base64_authorization_header.split(":")
+        result = decoded_base64_authorization_header.split(":", 1)
         key = result[0]
         value = result[1]
         return key, value
