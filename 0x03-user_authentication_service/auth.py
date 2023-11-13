3a4365b65ded268627f540c4b3ed83298c8ba7f0
grayolaRSA <gtndanga@gmail.com>
2023-11-13 23:47:52 +0200

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,36 @@
 
 import bcrypt
 from bcrypt import hashpw
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
     """function that hashes passwords"""
     return hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        method to register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            hashed_pw = _hash_password(password)
+
+            new_user = self._db.add_user(email, hashed_pw)
+
+            self._db._session.add(new_user)
+            self._db._session.commit()
+            return new_user
