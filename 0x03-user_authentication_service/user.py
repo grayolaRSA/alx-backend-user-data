2910c5158c56323e9e2643835d7252bd3215de89
grayolaRSA <gtndanga@gmail.com>
2023-11-13 19:06:15 +0200

    update

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+module to create a SQLAlchemy model for User class in users table
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User class declaration for SQL table
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
