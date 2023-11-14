cfe57147f78b7166fbc520007aaee982f47fc079
grayolaRSA <gtndanga@gmail.com>
2023-11-15 00:00:33 +0200

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 
 
 from os import getenv
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from typing import Dict
 from auth import Auth
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 
 app = Flask(__name__)
@@ -19,7 +21,7 @@ def hello_world() -> Dict:
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def users(email: str, password: str) -> Dict:
+def users() -> Dict:
     """function for registering a user"""
     email = request.form.get("email")
     password = request.form.get("password")
@@ -29,6 +31,21 @@ def users(email: str, password: str) -> Dict:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> Session:
+    """ function for logging in user and generating a session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = make_response({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+
+    abort(401)
 
 
 if __name__ == "__main__":
