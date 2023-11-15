934e87f0c2857d7f45f3203468c6bdb545c234a1
grayolaRSA <gtndanga@gmail.com>
2023-11-15 19:46:50 +0200

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,5 +74,22 @@ def profile() -> Dict:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_token() -> Dict:
+    """method to get a reset token after someone
+    loses their password"""
+    email = request.form.get("email")
+
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        if user.email == email:
+            reset_token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": user.email, "reset_token": reset_token})
+        abort(403)
+
+    except NoResultFound:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
