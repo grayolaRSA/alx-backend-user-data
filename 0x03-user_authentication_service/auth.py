b9ce4d159990c62f020a3813ebfdfc5beb0056f1
grayolaRSA <gtndanga@gmail.com>
2023-11-15 19:33:47 +0200

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,3 +90,16 @@ class Auth:
             self._db._session.commit()
         except NoResultFound:
             return ValueError('no related user ID found')
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        method to generate reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = self._generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+
+        except NoResultFound:
+            raise ValueError
